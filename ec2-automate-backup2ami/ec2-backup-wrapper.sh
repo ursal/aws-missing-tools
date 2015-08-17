de00d169d44363c7831b86abf3e849bf63e7c3ec
jazzl0ver <jazzl0ver@users.noreply.github.com>
2015-08-17 19:26:57 +0300

    Update ec2-backup-wrapper.sh

--- ec2-automate-backup2ami/ec2-backup-wrapper.sh
+++ ec2-automate-backup2ami/ec2-backup-wrapper.sh
@@ -5,14 +5,21 @@ MODE=$1
 
 BACKUP_SCRIPT=/usr/local/bin/ec2-automate-backup2ami.sh
 LOG=ec2-automate-backup2ami.$MODE.log
+LOG_ERR=ec2-automate-backup2ami.$MODE.err
 EMAILS=$2
+
+source /etc/profile.d/ec2-api.sh
 
 source .$MODE
 
-$BACKUP_SCRIPT -s tag -t "Backup=true" -k 14d -y -p -h -u -n &> $LOG
+$BACKUP_SCRIPT -s tag -t "Backup=true" -k 14d -p -h -u -n -y "Copy=true" -o "us-west-1 eu-west-1" > $LOG 2>$LOG_ERR
 RES=$?
 
 if [ ! "x$RES" = "x0" ]; then
 #    cat $LOG | grep -q UnauthorizedOperation &&
     cat $LOG | mail -s "EC2 AMI auto-backup $MODE: failed" $EMAILS
+fi
+
+if [ -s $LOG_ERR ]; then
+    cat $LOG_ERR | mail -s "EC2 AMI auto-backup $MODE: failed" $EMAILS
 fi
